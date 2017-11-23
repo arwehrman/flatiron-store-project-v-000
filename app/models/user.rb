3bbf1bc4c02fc3e4802a00cb16997b21aa33a46a
Amanda Wehrman <arwehrman@gmail.com>
2017-11-23 03:12:39 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,8 @@ class User < ActiveRecord::Base
   has_many :carts
   belongs_to :current_cart, class_name: 'Cart'
 
-  def create_current_cart
-    new_cart = carts.create
-    self.current_cart_id = new_cart.id
-    save
+  def current_cart
+    Cart.find_by(id: self.current_cart_id)
   end
 
-  def remove_current_cart
-    self.current_cart_id = nil
-    save
-  end
 end
