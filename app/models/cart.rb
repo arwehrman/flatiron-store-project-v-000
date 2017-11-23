a18f14dc72706f147e029027609c4fd8a58c2b7d
Amanda Wehrman <arwehrman@gmail.com>
2017-11-23 03:11:48 +0000

    automatically backed up by learn

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,30 +2,21 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
-
   def total
-    total = 0
-    self.line_items.each do |line_item|
-      total += line_item.item.price * line_item.quantity
+      total = 0
+      self.line_items.each { |line_item|
+        total = total + (line_item.quantity * line_item.item.price)
+      }
+      total
     end
-    return total
-  end
 
-  def add_item(item_id)
-    @line_item = self.line_items.find_by(item_id: item_id)
-    if @line_item
-      @line_item.quantity = @line_item.quantity + 1
-      @line_item
-    else
-      @line_item = LineItem.new(cart_id: self.id, item_id: item_id, quantity: 1)
+    def add_item(item_id)
+      if self.line_items.find_by(item_id: item_id, cart_id: self.id)
+        line_item = self.line_items.find_by(item_id: item_id, cart_id: self.id)
+        line_item.quantity += 1 unless line_item.item.inventory == 0
+      else
+        line_item = self.line_items.new(item_id: item_id, cart_id: self.id, quantity: 1)
+      end
+      line_item
     end
-  end
-
-  def checkout
-    self.status = "submitted"
-    self.line_items.each do |line_item|
-      line_item.item.inventory = line_item.item.inventory - line_item.quantity
-      line_item.item.save
-    end
-  end
 end
