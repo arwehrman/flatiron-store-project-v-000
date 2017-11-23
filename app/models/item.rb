4ea9c1ab671aac19d739b3b01a4f717e84f7e0f5
Amanda Wehrman <arwehrman@gmail.com>
2017-11-23 03:19:08 +0000

    automatically backed up by learn

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
-  belongs_to :category
   has_many :line_items
   belongs_to :category
+  has_many :carts, through: :line_items
 
   def self.available_items
    self.where("inventory > ?", 0)
