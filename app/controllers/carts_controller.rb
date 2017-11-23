bb421d5434b76775c175237d9d735199bbc1aba1
Amanda Wehrman <arwehrman@gmail.com>
2017-11-23 02:06:59 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,12 @@
 class CartsController < ApplicationController
+  def show
+    @cart = Cart.find(params[:id])
+  end
 
+  def checkout
+    @cart = Cart.find(params[:id])
+    @cart.checkout
+    current_user.remove_current_cart
+    redirect_to cart_path(@cart)
+  end
 end
